6f252c242ea7780e25586976829b291eb224fa0c
iluwQaa <iluwQaa@users.noreply.github.com>
2014-12-25 18:59:20 +0200

    Create clicker.js
    updated version 2.1

---
+++ clicker.js
@@ -0,0 +1,10 @@
+var time = $('div.event-head-keyttl').html();
+if($('event-head-keys__chance').length>0) {
+  console.log('Form found! Try submit...');
+  $('form').submit();
+} else {
+	console.log('Not found! Time left: '+time);
+setTimeout(function () {
+location.reload();
+}, 60000);
+}
