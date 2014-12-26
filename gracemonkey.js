a3f2321265146300a9b464996564bf0df08c8d6c
iluwka <mscorec@gmail.com>
2014-12-26 13:05:37 +0200

    added Gracemonkey script

--- gracemonkey.js
+++ gracemonkey.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name        GM KeyGraber
+// @name        GM WinterIndieFest KeyGraber
 // @namespace   gameminer.ru
 // @description key graber for winter indi fest
 // @version     2.1
 // @grant       none
-// @require		http://static.gameminer.ru/static/js/jquery-1.7.1.min.js
+// @require	http://static.gameminer.ru/static/js/jquery-1.7.1.min.js
 // @include     http://event.gameminer.ru/*
 // ==/UserScript==
 $(document).ready(function() {
