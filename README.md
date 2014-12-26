214e72bb774d32c49b2bb605f46d3d86daab47a3
iluwka <mscorec@gmail.com>
2014-12-26 10:41:37 +0200

    Merge branch 'master' of github.com:iluwQaa/gm
    Conflicts:
    	clicker.js

---
+++ README.md
@@ -0,0 +1,3 @@
+gm
+==
+some shity code for GameMiner Winter Indie Fest to grab some keys
--- clicker.js
+++ clicker.js
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 $(document).ready(function() {
 var time = $('div.event-head-keyttl').html();
 if($('.event-head-keys__chance').length>0) {
+=======
+var time = $('div.event-head-keyttl').html();
+if($('event-head-keys__chance').length>0) {
+>>>>>>> f4bf8d14b8fa95969dc6bb04687987e6d8d96c0b
   console.log('Form found! Try submit...');
   $('form').submit();
 } else {
@@ -9,4 +14,7 @@ setTimeout(function () {
 location.reload();
 }, 60000);
 }
+<<<<<<< HEAD
 	});
+=======
+>>>>>>> f4bf8d14b8fa95969dc6bb04687987e6d8d96c0b
