adae9c0b273d582e91f7c2310017484f1429d32f
iluwka <iluwQaa@users.noreply.github.com>
2016-07-06 15:54:52 +0300

    Delete steam.recomendations-clicker.js
    missclick :D

--- steam.recomendations-clicker.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() { 
-var element = document.getElementById("refresh_queue_btn");
-		if(element && element.click) {
-  			 element.click();
-		};
-	$('#next_in_queue_form').submit();
-});
